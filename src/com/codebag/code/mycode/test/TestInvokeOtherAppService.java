43948fa5ea967777a72324f75cdcea5b140ea2c3
zhangrui <zhangrui@maxthon.net>
2014-05-28 19:35:56 +0800

    加入测试调用其他应用的activity，service

---
+++ src/com/codebag/code/mycode/test/TestInvokeOtherAppService.java
@@ -0,0 +1,26 @@
+
+package com.codebag.code.mycode.test;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.codebag.bag.CaseListView;
+
+public class TestInvokeOtherAppService extends CaseListView {
+
+	public TestInvokeOtherAppService(Context context) {
+		super(context);
+	}
+	
+	public void run_otherService() {
+		Toast.makeText(getContext(), "run_otherActivity", Toast.LENGTH_SHORT).show();
+		Intent intent = new Intent();
+		ComponentName componentName = new ComponentName("com.lockscreen", "com.lockscreen.LockService");
+		intent.setComponent(componentName);
+		getContext().startService(intent);
+	}
+	
+}
+
