dacdc9aa949d779c9e9fd137f8826e6514d2536c
zhangrui <zhangrui@maxthon.net>
2014-06-25 18:50:38 +0800

    大图片中显示横竖滚动条

---
+++ src/com/codebag/code/mycode/view/scrollview/Example.java
@@ -0,0 +1,43 @@
+package com.codebag.code.mycode.view.scrollview;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.webkit.WebView;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+import com.codebag.R;
+import com.codebag.bag.CaseListView;
+import com.codebag.bag.Entry;
+
+public class Example extends CaseListView {
+
+	public Example(Context context) {
+		super(context);
+	}
+
+	@Entry()
+	public void show1() {
+		
+		HorizontalScrollView h = new HorizontalScrollView(getContext());
+		ScrollView v = new ScrollView(getContext());
+		ImageView iv = new ImageView(getContext());
+		iv.setImageResource(R.drawable.pic);
+		
+		h.addView(iv);
+		v.addView(h);
+		
+		showView(v);
+	}
+	@Entry()
+	public void show2() {
+		
+		WebView wv = new WebView(getContext());
+		String url = "file:///android_asset/pip.jpg";
+		wv.loadUrl(url);
+		showView(wv);
+	}
+}
