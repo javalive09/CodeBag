502374cf1e4997e233ce8aec4c5159981cd3bc73
zhangrui-ms <zhangrui-ms@zhangrui-ms-D1.corp.qihoo.net>
2014-07-29 19:16:54 +0800

    text draw

---
+++ src/com/codebag/code/mycode/view/textviewdraw/Invoker.java
@@ -0,0 +1,23 @@
+package com.codebag.code.mycode.view.textviewdraw;
+
+import android.content.Context;
+import android.graphics.Color;
+
+import com.codebag.bag.CaseListView;
+import com.codebag.bag.Entry;
+
+public class Invoker extends CaseListView {
+
+	public Invoker(Context context) {
+		super(context);
+	}
+	
+	@Entry
+	public void show() {
+		MyTextView mv = new MyTextView(getContext());
+		mv.setText("123");
+		mv.setTextColor(Color.BLUE);
+		showView(mv);
+	}
+
+}
---
+++ src/com/codebag/code/mycode/view/textviewdraw/MyTextView.java
@@ -0,0 +1,20 @@
+package com.codebag.code.mycode.view.textviewdraw;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.widget.TextView;
+
+public class MyTextView extends TextView {
+
+	public MyTextView(Context context) {
+		super(context);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+	}
+	
+	
+
+}
