d7a9de11ce54c83a407487f2d9396d14bcbca74d
zhangrui-ms <zhangrui-ms@zhangrui-ms-D1.corp.qihoo.net>
2014-09-24 09:39:58 +0800

    加入生成图标的demo

---
+++ src/com/codebag/code/mycode/function/shortcut/ShortCut.java
@@ -0,0 +1,33 @@
+package com.codebag.code.mycode.function.shortcut;
+
+import android.content.Intent;
+import android.os.Parcelable;
+
+import com.codebag.R;
+import com.codebag.bag.Entry;
+import com.codebag.bag.MainActivity;
+import com.codebag.bag.MyCode;
+
+public class ShortCut extends MyCode{
+
+	public ShortCut(MainActivity act) {
+		super(act);
+	}
+
+	@Entry
+	public void createShortCut() {
+		Intent shortcutintent = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
+		//不允许重复创建
+		shortcutintent.putExtra("duplicate", false);
+		//名称
+		shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_NAME, "CodeBag");
+		//图标
+		Parcelable icon = Intent.ShortcutIconResource.fromContext(getActivity().getApplicationContext(), R.drawable.head);
+		shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
+		//activity 入口
+		shortcutintent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, new Intent(getActivity().getApplicationContext() , MainActivity.class));
+		//广播
+		getActivity().sendBroadcast(shortcutintent);
+	}
+	
+}
