a97ffcc416ec3b72473538873fb907c897d9ca86
peter <peter@peterdeMacBook-Pro.local>
2017-01-24 14:56:58 +0800

    add adb shell wm

---
+++ sample/src/main/java/com/javalive09/sample/project/raventech/StringPid.java
@@ -0,0 +1,28 @@
+package com.javalive09.sample.project.raventech;
+
+import android.text.TextUtils;
+
+import com.javalive09.codebag.Entry;
+import com.javalive09.codebag.LogUtil;
+
+/**
+ * Created by peter on 2016/12/12.
+ */
+
+public class StringPid extends Entry {
+
+    public void getName() {
+        String str = "u0_a59    11142 3583  1740208 157280 SyS_epoll_ f709a1cc S net.myvst.v2:vstmain";
+        String[] ids = str.split(" ");
+
+        for(String id: ids) {
+            if(!TextUtils.isEmpty(id) && TextUtils.isDigitsOnly(id)) {
+                LogUtil.e(id);
+                break;
+            }
+
+        }
+    }
+
+
+}
