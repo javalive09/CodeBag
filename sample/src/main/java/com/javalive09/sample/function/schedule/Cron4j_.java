ab4bf90c8e4b10a1d7841f6a02d8e853a926f22a
peterzhang <peterzhang@raventech.com>
2017-03-20 17:26:51 +0800

    modify cron

--- sample/src/main/java/com/javalive09/sample/function/schedule/Cron4j_.java
+++ sample/src/main/java/com/javalive09/sample/function/schedule/Cron4j_.java
@@ -1,23 +1,24 @@
 package com.javalive09.sample.function.schedule;
 
+import android.text.TextUtils;
+
 import com.javalive09.codebag.Entry;
 import com.javalive09.codebag.LogUtil;
+
+import java.util.Calendar;
 
 import it.sauronsoftware.cron4j.Scheduler;
 
 /**
- *
  * Created by peter on 2017/3/15.
- *
- *   +---------------- minute (0 - 59)
- *   |  +------------- hour (0 - 23)
- *   |  |  +---------- day of month (1 - 31)
- *   |  |  |  +------- month (1 - 12)
- *   |  |  |  |  +---- day of week (0 - 6) (Sunday=0 or 7)
- *   |  |  |  |  |
- *   *  *  *  *  *  command to be executed
- *
- *
+ * <p>
+ * +---------------- minute (0 - 59)
+ * |  +------------- hour (0 - 23)
+ * |  |  +---------- day of month (1 - 31)
+ * |  |  |  +------- month (1 - 12)
+ * |  |  |  |  +---- day of week (0 - 6) (Sunday=0 or 7)
+ * |  |  |  |  |
+ * *  *  *  *  *  command to be executed
  */
 
 public class Cron4j_ extends Entry {
@@ -25,7 +26,7 @@ public class Cron4j_ extends Entry {
     private Scheduler scheduler = new Scheduler();
     private String schedulingPattern1 = "30 10 * * *"; // 10:30  everyday
     private String schedulingPattern2 = "* * * * *"; // every minute
-    private int count =0;
+    private int count = 0;
 
     public void schedule() {
         scheduler.schedule(schedulingPattern2, new Runnable() {
@@ -41,5 +42,76 @@ public class Cron4j_ extends Entry {
     public void cancel() {
         scheduler.stop();
     }
+
+
+    public String scheduleDay() {
+        String repeat = "day";
+        Calendar instance = Calendar.getInstance();
+        instance.setTimeInMillis(1489639200 * 1000l);
+        int month = instance.get(Calendar.MONTH) + 1;
+        int day = instance.get(Calendar.DAY_OF_MONTH);
+        int hour = instance.get(Calendar.HOUR_OF_DAY);
+        int minute = instance.get(Calendar.MINUTE);
+        String pattern = "";
+        if (TextUtils.isEmpty(repeat)) {
+            pattern = minute + " " + hour + " " + day + " " + month + " *";
+        } else {
+            if ("day".equals(repeat)) {
+                pattern = minute + " " + hour + " *" + " *" + " *";
+            } else if ("month".equals(repeat)) {
+                pattern = minute + " " + hour + " " + day + " *" + " *";
+            } else if ("year".equals(repeat)) {
+                pattern = minute + " " + hour + " " + day + " " + month + " *";
+            }
+        }
+        return pattern;
+    }
+
+    public String scheduleMonth() {
+        String repeat = "month";
+        Calendar instance = Calendar.getInstance();
+        instance.setTimeInMillis(1489639200 * 1000l);
+        int month = instance.get(Calendar.MONTH) + 1;
+        int day = instance.get(Calendar.DAY_OF_MONTH);
+        int hour = instance.get(Calendar.HOUR_OF_DAY);
+        int minute = instance.get(Calendar.MINUTE);
+        String pattern = "";
+        if (TextUtils.isEmpty(repeat)) {
+            pattern = minute + " " + hour + " " + day + " " + month + " *";
+        } else {
+            if ("day".equals(repeat)) {
+                pattern = minute + " " + hour + " *" + " *" + " *";
+            } else if ("month".equals(repeat)) {
+                pattern = minute + " " + hour + " " + day + " *" + " *";
+            } else if ("year".equals(repeat)) {
+                pattern = minute + " " + hour + " " + day + " " + month + " *";
+            }
+        }
+        return pattern;
+    }
+
+    public String scheduleYear() {
+        String repeat = "year";
+        Calendar instance = Calendar.getInstance();
+        instance.setTimeInMillis(1489639200 * 1000l);
+        int month = instance.get(Calendar.MONTH) + 1;
+        int day = instance.get(Calendar.DAY_OF_MONTH);
+        int hour = instance.get(Calendar.HOUR_OF_DAY);
+        int minute = instance.get(Calendar.MINUTE);
+        String pattern = "";
+        if (TextUtils.isEmpty(repeat)) {
+            pattern = minute + " " + hour + " " + day + " " + month + " *";
+        } else {
+            if ("day".equals(repeat)) {
+                pattern = minute + " " + hour + " *" + " *" + " *";
+            } else if ("month".equals(repeat)) {
+                pattern = minute + " " + hour + " " + day + " *" + " *";
+            } else if ("year".equals(repeat)) {
+                pattern = minute + " " + hour + " " + day + " " + month + " *";
+            }
+        }
+        return pattern;
+    }
+
 
 }
