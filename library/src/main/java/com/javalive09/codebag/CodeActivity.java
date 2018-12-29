cba37f4a82d877c021e694d7f83d6db00c0433f3
zhangrui <zhangrui24@baidu.com>
2018-12-29 15:44:21 +0800

    clear code

--- library/src/main/java/com/javalive09/codebag/CodeActivity.java
+++ library/src/main/java/com/javalive09/codebag/CodeActivity.java
@@ -5,12 +5,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.StringRes;
 import android.text.TextUtils;
-import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -18,13 +15,8 @@ import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-
-import com.javalive09.annotation.Code;
 import com.javalive09.annotation.Run;
 
 /**
